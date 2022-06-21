4cc05bc1a4c2971670d8ceeef7772365df4a6888
agnaldosantos1935 <agnaldosantos1935@gmail.com>
2022-06-20 22:14:53 -0300

    Atualização

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,18 +12,31 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="{{asset('/vendor/adminlte/dist/css/style.css')}}">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     </head>
     <body class="antialiased">
-
-    <nav class="navbar navbar-inverse">
+    <div class="header">
+  <a href="#default" class="logo">Sigesco</a>
+  <div class="header-right">
+    <a class="active" href="#home">Ouvidoria/Fale conosco</a>
+    <a href="#contact">Transparência</a>
+    <a href="#about">Contatos</a>
+  </div>
+</div>
+      <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
+      <!--<a class="navbar-brand" href="#">Sigesco</a> -->
     </div>
     <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li>
+    <li class="active"><a href="#">Home</a></li>
+    <li class="active"><a href="#">Organograma</a></li>
+      <li class="active"><a href="#">Institucional</a></li>
+      <li><a href="#">Secretarias</a></li>
+      <li><a href="#">Serviços on line</a></li>
+      <li><a href="#">Editais/Licitações</a></li>
+      <li><a href="#">Acesso à informação</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
     @if (Route::has('login'))
@@ -32,11 +45,11 @@
       <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
         @else
         
-      <li><a href="{{ route('login') }}"><span class="glyphicon glyphicon-log-in"></span>Entrar</a></li>
+      <li><a href="{{ route('login') }}"><span class="glyphicon glyphicon-log-in"></span>Intranet</a></li>
 
 @if (Route::has('register'))
     
-    <li><a href="{{ route('register') }}"><span class="glyphicon glyphicon-user"></span> Registrar</a></li>
+    <!-- <li><a href="{{ route('register') }}"><span class="glyphicon glyphicon-user"></span> Registrar</a></li> -->
 @endif
 @endauth
 </div>
@@ -46,8 +59,10 @@
 </nav>
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col sm 12">
-                    <h2>Carousel Example</h2>  
+                <div class="col sm 1">
+                </div>
+                    <div class="col sm 10">
+                   
   <div id="myCarousel" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
     <ol class="carousel-indicators">
@@ -59,15 +74,15 @@
     <!-- Wrapper for slides -->
     <div class="carousel-inner">
       <div class="item active">
-        <img src="{{asset('/vendor/adminlte/dist/img/escola1.jpg')}}" alt="Los Angeles" style="width:100%;">
+        <img class="img_carrosel" src="{{asset('/vendor/adminlte/dist/img/escola1.jpg')}}" alt="Imagem 1" >
       </div>
 
       <div class="item">
-        <img src="chicago.jpg" alt="Chicago" style="width:100%;">
+        <img class="img_carrosel" src="{{asset('/vendor/adminlte/dist/img/escola1.jpg')}}" alt="Imagem 2" >
       </div>
     
       <div class="item">
-        <img src="ny.jpg" alt="New york" style="width:100%;">
+        <img class="img_carrosel" src="{{asset('/vendor/adminlte/dist/img/escola1.jpg')}}" alt="Imagem 3" >
       </div>
     </div>
 
@@ -81,6 +96,8 @@
       <span class="sr-only">Next</span>
     </a>
   </div>
+                    </div>
+                    <div class="col sm 1">
                     </div>
                 </div>
             </div>
