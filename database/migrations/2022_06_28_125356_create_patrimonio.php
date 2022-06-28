92f2fe4f632c427d8e43271b96fe3f68c3c2b648
AgnaldoSantos1935 <agnaldosantos1935@gmail.com>
2022-06-28 10:02:01 -0300

    atualização

---
+++ database/migrations/2022_06_28_125356_create_patrimonio.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatrimonio extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patrimonio', function (Blueprint $table) {
+            $table->id();
+            $table->string('nomeobjeto');
+            $table->string('serial');
+            $table->timestamp('rpnum')->nullable();
+            $table->string('modelo');
+            $table->string('dtaquicao');
+            $table->string('origem');
+            $table->string('estadodeconservacao');
+            $table->string('depreciacaoanual');
+            $table->string('cores');
+            $table->string('tamanho');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patrimonio');
+    }
+}
